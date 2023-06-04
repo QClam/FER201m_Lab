1fe1e2f54048731f90b8c43bae0f10c401e89d07
Xuan Quy <hoangxuanquy5@gmail.com>
2023-06-04 16:59:53 +0700

    lab2

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,50 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
+import Switch from 'react-switch';
 
 export default function Navigation() {
+  const { theme, toggle, dark } = useContext(ThemeContext);
+
   return (
-    <nav>
-      <ul className='navigation-list'>
-        <li>
-          <a className='active' href='#home'>Trang chủ</a>
-        </li>
-        <li>
-          <a className='active' href='#news'>Giới thiệu</a>
-        </li>
-        <li>
-          <a className='active' href='#about'>Dịch vụ</a>
-        </li>
-        <li>
-          <a className='active' href='#contact'>Liên hệ</a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav style={{ backgroundColor: theme.backgroundColor, color: theme.color }}>
+        <ul className='navigation-list'>
+          <li>
+            <a style={{ backgroundColor: theme.backgroundColor, color: theme.color }} className='active' href='#home'>Trang chủ</a>
+          </li>
+          <li>
+            <a style={{ backgroundColor: theme.backgroundColor, color: theme.color }} className='active' href='#news'>Giới thiệu</a>
+          </li>
+          <li>
+            <a style={{ backgroundColor: theme.backgroundColor, color: theme.color }} className='active' href='#about'>Dịch vụ</a>
+          </li>
+          <li>
+            <a style={{ backgroundColor: theme.backgroundColor, color: theme.color }} className='active' href='#contact'>Liên hệ</a>
+          </li>
+          <li>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <label htmlFor="toggle-theme-btn" style={{ marginRight: '10px', fontSize: '14px' }}>
+                Switch to {!dark ? 'Dark' : 'Light'} mode
+              </label>
+              <Switch
+                checked={dark}
+                onChange={toggle}
+                onColor="#000"
+                onHandleColor="#fff"
+                handleDiameter={24}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                activeBoxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                height={16}
+                width={40}
+                className="react-switch"
+                id="toggle-theme-btn"
+              />
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
